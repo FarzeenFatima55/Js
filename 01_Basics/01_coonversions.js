4dd54831540b70ea3ac66abdcc14a6c055961f8c
FarzeenFatima55 <fatimafarzeen675@gmail.com>
2025-07-26 13:01:22 +0000

    conversions of datatypes

---
+++ 01_Basics/01_coonversions.js
@@ -0,0 +1,21 @@
+let score = "33"
+
+console.log(typeof (score));
+let valueInNumber = Number(score);
+console.log(typeof ValueInNumber);
+//console.log(typeof (ValueInNumber));
+
+// "33" => 33
+//  "33abc" => Nan
+//true =>1; fasle=>0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//"" =>false
+//"fefee" => true
+
+let number = 33
+let stringnum = String(number)
+console.log(typeof stringnum);
